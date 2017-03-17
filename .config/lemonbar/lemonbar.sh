ec48c274e003392ffe6a060e81107776acdf43c2
Jeff Shen <jshen@protonmail.ch>
2017-03-16 21:38:42 -0700

    files updated

--- .config/lemonbar/lemonbar.sh
+++ .config/lemonbar/lemonbar.sh
@@ -5,32 +5,39 @@ clock() {
 }
 
 volume() {
-    amixer get Master|grep Mono |sed 's/[^f]//g' > /home/js/.config/lemonbar/mutefile
-    python /home/js/.config/lemonbar/mute.py
+#    amixer get Master|grep Mono |sed 's/[^f]//g' > /home/js/.config/lemonbar/mutefile
+#    python /home/js/.config/lemonbar/mute.py
     amixer get Headphone | sed -n 's/^.*\[\([0-9]\+\)%.*$/\1/p'| uniq
 }
 
-cpu_usage() {
-    USAGE=`ps -eo pcpu |grep -vE '^\s*(0.0|%CPU)' |sed -n '1h;$!H;$g;s/\n/ +/gp'`
-    bc <<< $USAGE
-}
+#cpu_usage() {
+#    USAGE=`ps -eo pcpu |grep -vE '^\s*(0.0|%CPU)' |sed -n '1h;$!H;$g;s/\n/ +/gp'`
+#    bc <<< $USAGE
+#}
 
-ram_usage() {
-    screenfetch -nN |grep --color=never RAM > /home/js/.config/lemonbar/ramfile
-    sed 's/[^0-9/ ]*//g' /home/js/.config/lemonbar/ramfile > /home/js/.config/lemonbar/ramin
-    python /home/js/.config/lemonbar/rampercentage.py
-}
+#ram_usage() {
+#    screenfetch -nN |grep --color=never RAM > /home/js/.config/lemonbar/ramfile
+#    sed 's/[^0-9/ ]*//g' /home/js/.config/lemonbar/ramfile > /home/js/.config/lemonbar/ramin
+#    python /home/js/.config/lemonbar/rampercentage.py
+#}
 
 network() {
-    iw dev wlp0s20u9 link |grep --color=never onnected > /home/js/.config/lemonbar/netfile
+    iw dev net0 link |grep --color=never onnected > /home/js/.config/lemonbar/netfile
     python /home/js/.config/lemonbar/netstatus.py
 }
 
 song() {
-    mpc |grep - 
+    mpc current
 }
 
+repeat() {
+    mpc |grep repeat > /home/js/.config/lemonbar/repeat
+    python2 /home/js/.config/lemonbar/repeat.py
+}
+
+#CPU: $(cpu_usage)%   |   RAM: $(ram_usage)  
+
 while true; do
-    echo "%{B#424242}%{F#00BFA5}%{l}   $(network)   |   CPU: $(cpu_usage)%   |   RAM: $(ram_usage)   |   VOL: $(volume)% %{c}$(song) %{r}$(clock)     "
+    echo "%{B#424242}%{F#00BFA5}%{l}   $(network)   |   VOL: $(volume)% %{c}$(song) $(repeat) %{r}$(clock)     "
     sleep 1
 done
