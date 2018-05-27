bcf6962346a92800151e1b67fd552dd400934a2a
Jeff Shen <jshen@protonmail.ch>
2018-05-27 10:15:17 -0700

    27 May 2018 V2

--- .config/lemonbar/lemonbar.sh
+++ .config/lemonbar/lemonbar.sh
@@ -37,16 +37,8 @@ repeat() {
     python2 /home/js/.config/lemonbar/repeat.py
 }
 
-ethprice() {
-    cat /home/js/.config/lemonbar/cryptoprice/ethprice
-}
-
-btcprice() {
-    cat /home/js/.config/lemonbar/cryptoprice/btcprice
-}
-
 while true; do
-    echo "%{B#262626}%{F#C4C4C4}%{l}   $(network) %{F#20C20E} $(vpn)%{F#C4C4C4}| VOL: $(volume)% $(audioOut) %{c}%{F#3ECAE8}$(song) $(repeat) %{r}%{F#E84A5F} $(temps)%{F#C4C4C4} $(ethprice) $(btcprice) | $(clock)   "
+    echo "%{B#262626}%{F#C4C4C4}%{l}   $(network) %{F#20C20E} $(vpn)%{F#C4C4C4}| VOL: $(volume)% $(audioOut) %{c}%{F#3ECAE8}$(song) $(repeat) %{r}%{F#E84A5F} $(temps)%{F#C4C4C4} | $(clock)   "
     sleep 1
 done
 
